ba1c20fcc4aa1b51aa8916533a4fd24c03119f77
FelipeCardozo96 <felipemcr996@gmail.com>
2023-07-25 21:33:03 -0300

    Las rutas de imagenes/logo fueron modificadas

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express")
 const app = express()
-const path = require('path')
-app.use(express.static("public"))
-
+const path = require("path")
+const localPath = (path.join(__dirname, "public"))
+app.use(express.static(localPath))
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo");
   })
 
-app.get("/", function (req, res) {
+let rutaHome = path.join(__dirname, "/views/home.html")
+app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
